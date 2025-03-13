ea6d1502af5abefc73cf339be52c9ecb731aeeb3
Anand Kushwaha <anandsanin09@gmail.com>
2025-03-13 10:52:21 +0530

    sign up and login commit

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ import router from "./routes/authRoutes.js";
 app.use("/api/auth", router);
 app.use(
   cors({
-    origin: "http://127.0.0.1:5500,https://gamingforge-frontend.vercel.app", // Allow your frontend URL
+    origin:
+      "http://127.0.0.1:5500,https://gamingforge-frontend.vercel.app,https://gamingforge-frontend.vercel.app", // Allow your frontend URL
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed request methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
   })
